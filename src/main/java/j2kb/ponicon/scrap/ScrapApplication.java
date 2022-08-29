86b7c593d222a00393bd44f22397c63c2666af7f
choign3123 <choign3123@naver.com>
2022-08-29 14:11:50 +0900

    초기 세팅

---
+++ src/main/java/j2kb/ponicon/scrap/ScrapApplication.java
@@ -0,0 +1,13 @@
+package j2kb.ponicon.scrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapApplication.class, args);
+	}
+
+}
---
+++ src/main/java/j2kb/ponicon/scrap/TestController.java
@@ -0,0 +1,16 @@
+package j2kb.ponicon.scrap;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("")
+    public String test(){
+        return "스크랩 테스트 성공";
+    }
+}
