555bd28a9b155c5f40b4f18c87691cc2664daa34
choign3123 <choign3123@naver.com>
2022-08-29 20:27:08 +0900

    도메인 수정-아이디에 unique 적용, 주석 작성

--- src/main/java/j2kb/ponicon/scrap/domain/Category.java
+++ src/main/java/j2kb/ponicon/scrap/domain/Category.java
@@ -16,19 +16,23 @@ public class Category {
     @Column(name = "category_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @OneToMany(mappedBy = "category")
-    private List<Link> links = new ArrayList<>();
-
+    // 카테고리 이름
     @Column(length = 60, nullable = false)
     private String name;
 
+    // 카테고리 순서
     @Column(name = "category_order", nullable = false)
     private int order;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    // 유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 해당 카테고리에 속한 자료들
+    @OneToMany(mappedBy = "category")
+    private List<Link> links = new ArrayList<>();
 }
--- src/main/java/j2kb/ponicon/scrap/domain/Link.java
+++ src/main/java/j2kb/ponicon/scrap/domain/Link.java
@@ -14,25 +14,31 @@ public class Link {
     @Column(name = "link_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private Category category;
-
+    // 자료의 링크
     @Column(length = 300, nullable = false)
     private String link;
 
+    // 자료의 제목
     @Column(length = 100)
     private String title;
 
+    // 자료의 이미지 url
     @Column(length = 300)
     private String imgUrl;
 
+    // 즐겨찾기
     private boolean star = false;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    // 유저
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 카테고리
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
