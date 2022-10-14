729ad555287cf27a36a5c2a0d409809ea26e7b6c
choign3123 <choign3123@naver.com>
2022-10-15 00:46:47 +0900

    스프링 서버 시간 설정 확인용

--- src/main/java/j2kb/ponicon/scrap/ScrapApplication.java
+++ src/main/java/j2kb/ponicon/scrap/ScrapApplication.java
@@ -3,6 +3,8 @@ package j2kb.ponicon.scrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class ScrapApplication {
 
