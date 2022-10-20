297c70b9b722dd4df22c413da4161c1ef65320cc
choign3123 <choign3123@naver.com>
2022-10-20 16:29:53 +0900

    이메일 형식으로 변경, 토큰 만료시간 변경

--- src/main/java/j2kb/ponicon/scrap/response/validationSequence/ValidationSequence.java
+++ src/main/java/j2kb/ponicon/scrap/response/validationSequence/ValidationSequence.java
@@ -6,6 +6,6 @@ import javax.validation.groups.Default;
 import static j2kb.ponicon.scrap.response.validationSequence.ValidationGroup.*;
 
 // 왼 -> 오 순서로 적용됨
-@GroupSequence({Default.class, NotNullGroup.class, NotEmptyGroup.class, PatternGroup.class})
+@GroupSequence({Default.class, NotNullGroup.class, NotEmptyGroup.class, PatternGroup.class, EmailGroup.class})
 public interface ValidationSequence {
 }
--- src/main/java/j2kb/ponicon/scrap/user/dto/PostJoinReq.java
+++ src/main/java/j2kb/ponicon/scrap/user/dto/PostJoinReq.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
@@ -17,9 +18,10 @@ import static j2kb.ponicon.scrap.response.validationSequence.ValidationGroup.*;
 @NoArgsConstructor
 public class PostJoinReq {
 
-    @NotEmpty(message = "아이디를 입력해주세요", groups = NotEmptyGroup.class)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{5,15}$", message = "아이디 형식이 일치하지 않습니다", groups = PatternGroup.class)
-    private String username; // 아이디
+    @NotEmpty(message = "이메일을 입력해주세요", groups = NotEmptyGroup.class)
+//    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{5,15}$", message = "아이디 형식이 일치하지 않습니다", groups = PatternGroup.class)
+    @Email(message = "이메일 형식이 일치하지 않습니다", groups = EmailGroup.class)
+    private String email; // 아이디
 
     @NotEmpty(message = "비밀번호를 입력해주세요", groups = NotEmptyGroup.class)
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d!?@#$%&*]{5,15}$", message = "비밀번호 형식이 일치하지 않습니다", groups = PatternGroup.class)
