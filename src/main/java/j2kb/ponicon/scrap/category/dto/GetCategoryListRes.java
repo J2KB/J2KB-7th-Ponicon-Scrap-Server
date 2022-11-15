0091ac30d79ab1019d7f0a1e2d0c903de91dc169
lusida0131 <ks12b9189@naver.com>
2022-11-15 17:25:44 +0900

    프론트에서 imageUrl, title 받게 수정

--- src/main/java/j2kb/ponicon/scrap/category/dto/GetCategoryListRes.java
+++ src/main/java/j2kb/ponicon/scrap/category/dto/GetCategoryListRes.java
@@ -2,10 +2,12 @@ package j2kb.ponicon.scrap.category.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor // cache cannot deserialize from Object value 오류 때문에 추가함
 public class GetCategoryListRes {
     List<CategoryListRes> categories;
 
--- src/main/java/j2kb/ponicon/scrap/data/LinkService.java
+++ src/main/java/j2kb/ponicon/scrap/data/LinkService.java
@@ -1,13 +1,10 @@
 package j2kb.ponicon.scrap.data;
 
-import j2kb.ponicon.scrap.data.dto.GetDataListRes;
-import j2kb.ponicon.scrap.data.dto.PatchLinkReq;
-import j2kb.ponicon.scrap.data.dto.PostDataSaveRes;
-import j2kb.ponicon.scrap.data.dto.PostUrlReq;
+import j2kb.ponicon.scrap.data.dto.*;
 import j2kb.ponicon.scrap.domain.Link;
 
 public interface LinkService {
-    public PostDataSaveRes linkSave(PostUrlReq postUrlReq, Long userId, Long categoryId) throws Exception;
+    public PostDataSaveRes linkSave(PostDataSaveReq postDataSaveReq, Long userId, Long categoryId) throws Exception;
 
     public GetDataListRes links(Long userId, Long categoryId);
 
